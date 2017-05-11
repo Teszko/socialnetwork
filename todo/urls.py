93319c94495bc6cf25836f121acba3ccc98d7277
Henryk Iwaniuk <b.iwaniuk@campus.tu-berlin.de>
2017-05-11 20:36:22 +0200

    removed todoList url prefix

--- todo/urls.py
+++ todo/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-               url(r'^todoList/', include('todoList.urls')),
                url(r'^admin/', admin.site.urls),
+               url(r'^', include('todoList.urls')),
                ]
